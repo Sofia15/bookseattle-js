8fa25384fbcf4816c7f4dd64ced40f3223f9ed17
Sofia Kim <sofiamhkim@gmail.com>
2017-07-08 11:06:06 -0700

    Add initial search form.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import 'webcomponents.js/webcomponents-lite.js'; // polyfill
 import { Component } from 'panel';
 import { html } from 'snabbdom-jsx';
 import 'normalize.css';
+import 'skeleton-css/css/skeleton.css';
+import './index.css';
+import request from 'superagent';
 
 console.log('hello world');
 
@@ -15,7 +18,10 @@ document.registerElement('bookseattle-app', class extends Component {
       routes: {
         // 'bookseattle': () => ({$view: 'bookseattle'}),
         'home':   () => ({$view: 'home'}),
-        '':        'home',
+        'rooms/:name': (stateUpdate, name) => {
+          return ({$view: 'room'})
+        },
+        '':        'home'
       },
 
       template: state =>
@@ -33,7 +39,7 @@ document.registerElement('navigation-view', class extends Component {
       template: state =>
         <div>
           <ul>
-            <li><a href="#rooms">rooms</a></li>
+            <li><a href="#home">rooms</a></li>
           </ul>
         </div>
     };
@@ -44,9 +50,24 @@ document.registerElement('home-view', class extends Component {
   get config() {
     return {
       template: state =>
-        <div className="home">
-            bookseattle
+        <div className="container">
+            <img src="https://s3-us-west-2.amazonaws.com/www.bookseattle.net/rooms/banner.png" />
         </div>
     };
+  }
+});
+
+document.registerElement('room-view', class extends Component {
+  get config() {
+    return {
+      template: state =>
+      <div>
+        <form action="http://localhost:3000/availability" method="get">
+        <label for="check_in">Check In</label>
+        <input name="check_in" type="date"></input>
+        <label for="check_out">Check Out</label>
+        <input name="check_out" type="date"></input>
+        <input type="submit">Reserve</input>    
+      </div>
   }
 });
